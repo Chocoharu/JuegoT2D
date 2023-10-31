c90d83959b085b8ad0dd77ce041c0f88ba51957a
pabrojas_utalca <pabrojas.utalca@gmail.com>
2023-10-31 10:22:13 -0300

    brazo lanza tiza
    implementación tiza

---
+++ Assets/Script/arm.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arm : MonoBehaviour
+{
+    public Transform arm1;
+    public SpriteRenderer armR;
+    public int speedTiza;
+    Vector3 targetrotation;
+
+    public GameObject tiza;
+    Vector3 finalTarget;
+
+    void Update()
+    {
+        targetrotation = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = MathF.Atan2(targetrotation.y, targetrotation.x) * Mathf.Rad2Deg;
+        arm1.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        if(angle > 90 || angle < -90)
+            armR.flipY = true;
+        else
+            armR.flipY= false;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            shoot();
+    }
+    void shoot()
+    {
+        var Ball = Instantiate(tiza, arm1.position, transform.rotation, transform.parent);
+        targetrotation.z = 0;
+        finalTarget = (targetrotation - transform.position).normalized;
+        Ball.GetComponent<Rigidbody2D>().AddForce(finalTarget * speedTiza, ForceMode2D.Impulse);
+    }
+}
