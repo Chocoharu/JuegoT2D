6ae135d3b62767601d16c56065f7229887e30dd9
Chocoharu <achocobarbel@gmail.com>
2023-11-10 21:03:32 -0300

    Correccion direccion alumno

--- Assets/Script/MoviminetoAlumno.cs
+++ Assets/Script/MoviminetoAlumno.cs
@@ -6,17 +6,19 @@ using UnityEngine;
 public class MoviminetoAlumno : MonoBehaviour
 {
     public float velocidad = 2.0f;
+    private bool PosGeneracion;
     // Start is called before the first frame update
     void Start()
     {
+        PosGeneracion = transform.position.x < 0;
     }
 
     // Update is called once per frame
     void Update()
     {
       
-       if(transform.position.x>0) { transform.Translate(Vector2.left * velocidad * Time.deltaTime); }
-       else if(transform.position.x <= 0) { transform.Translate(Vector2.right * velocidad * Time.deltaTime); }
+       if(PosGeneracion) { transform.Translate(Vector2.right * velocidad * Time.deltaTime); }
+       else  { transform.Translate(Vector2.left * velocidad * Time.deltaTime); }
         
     }
 }
