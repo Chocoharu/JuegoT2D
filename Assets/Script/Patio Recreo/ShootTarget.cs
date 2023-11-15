7b6e9d25c9fbc7f9f3e8c816f4dffecace1a6934
Chocoharu <achocobarbel@gmail.com>
2023-11-15 19:49:36 -0300

    Mejoras click estudiantes

--- Assets/Script/Patio Recreo/ShootTarget.cs
+++ Assets/Script/Patio Recreo/ShootTarget.cs
@@ -6,12 +6,16 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class ShootTarget : MonoBehaviour
 {
+    public GameObject Tierra;
     private bool Inquieto = false;
+    private bool cambioColor = false;
+    private bool click = false;
     private float Timer = 3f;
     private Renderer renderer;
     public bool newBorn = false;
     public float StartInquieto;
     public bool Movimiento = true;
+    private static int ContTierra = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,17 +32,23 @@ public class ShootTarget : MonoBehaviour
         {
             float randomValue = Random.value;
 
-            if (randomValue <= 0.2f)
+            if (randomValue <= 0.2f&&!cambioColor&&!CompareTag("Ave"))
             {
                 Inquieto = true;
                 renderer.material.color = Color.red;
+                cambioColor = true;
             }
-            /*Timer -= Time.deltaTime;
-            if (Timer <= 0)
+            Timer -= Time.deltaTime;
+            if (Timer <= 0 || click)
             {
                 Inquieto = false;
                 renderer.material.color = Color.white;
-            }*/
+                if (!click && CompareTag("Estudiante")&& ContTierra < 3)
+                {
+                    Instantiate(Tierra, new Vector3(0, 0, 0), Quaternion.identity);
+                    ContTierra++;
+                }
+            }
         }
         else
         {
@@ -52,18 +62,21 @@ public class ShootTarget : MonoBehaviour
 
     private void OnMouseDown()
     {
+        if (click) return;
         if (CompareTag("Estudiante") && Inquieto)
         {
             PlaygroundShoot.Instance.AddPoint(100);
             this.gameObject.GetComponent<MoviminetoAlumno>().PermisoMovimiento = false;
-            /*Inquieto = false;
-            renderer.material.color = Color.white;*/
+            click = true;
+            
+            //renderer.material.color = Color.white;
             //Destroy(gameObject);
         }
         else if((CompareTag("Estudiante") && !Inquieto))
         {
             PlaygroundShoot.Instance.RemovePoint(100);
             this.gameObject.GetComponent<MoviminetoAlumno>().PermisoMovimiento = false;
+            click = true;
             //Destroy(gameObject);
         }
         else if (CompareTag("Ave"))
